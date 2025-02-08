309fefe16648421509912bc8537fc2052ccd9bd1
Jeferson Andrade <j3frsn@proton.me>
2025-02-08 13:54:46 -0500

    backup

--- backend/routes.py
+++ backend/routes.py
@@ -67,3 +67,45 @@ def count():
 def songs():
     songs = json_util.dumps(db.songs.find({}))
     return {"songs": songs}, 200
+
+
+@app.route("/song/<int:id>", methods=["GET"])
+def get_song_by_id(id):
+    song = db.songs.find_one({"id": id})
+
+    if not song:
+        return {"message": f"song with id <{id}> not found"}, 404
+
+    return {"song": json_util.dumps((song))}, 200
+
+
+# @app.route("/song", methods=["POST"])
+# def create_song():
+#     new_song = request.get_json()
+#     print("New song:\n" + new_song)
+
+#     found = db.songs.find_one({"id": new_song["id"]})
+
+#     if found:
+#         return {"Message": f"song with id {new_song['id']} already present"}, 302
+
+#     insert_id: InsertOneResult = db.songs.insert_one(new_song)
+#     return {"inserted id": parse_json(insert_id)}, 201
+
+@app.route("/song", methods=["POST"])
+def create_song():
+    # get data from the json body
+    song_in = request.json
+
+    print(song_in["id"])
+
+    # if the id is already there, return 303 with the URL for the resource
+    song = db.songs.find_one({"id": song_in["id"]})
+    if song:
+        return {
+            "Message": f"song with id {song_in['id']} already present"
+        }, 302
+
+    insert_id: InsertOneResult = db.songs.insert_one(song_in)
+
+    return {"inserted id": parse_json(insert_id.inserted_id)}, 201
